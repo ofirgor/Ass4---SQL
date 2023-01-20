807b57be2e7e1dd99d093a451a831b7942881019
ofirgor <118345817+ofirgor@users.noreply.github.com>
2023-01-20 18:41:07 +0200

    implemented third (and last) action in printdb.py: printing activities report TODO: check all instructions and test if is equal

--- printdb.py
+++ printdb.py
@@ -1,7 +1,7 @@
 from persistence import *
 
 def main():
-    # print all tables
+    # 1. print all tables
 
     tables_names = {k: v for k, v in repo.__dict__.items() if isinstance(v, Dao)}
     for table in sorted(tables_names):
@@ -12,18 +12,27 @@ def main():
         table_data = [[val.decode() if isinstance(val, bytes) else val for val in row] for row in table_data]
         for row in table_data:
             print(tuple(row))
-    # print employees report
+    # 2. print employees report
 
-    print("Employees report")
-    employees_data = repo.execute_command('SELECT employees.name, employees.salary, branches.location\
-                                        , (SELECT SUM(-quantity* (SELECT price FROM products WHERE products.id = activities.product_id))\
+    print('\n' + "Employees report")
+    employees_data = repo.execute_command('SELECT employees.name, employees.salary, branches.location,\
+                                         (SELECT SUM(-quantity* (SELECT price FROM products WHERE products.id = activities.product_id))\
                                         FROM activities WHERE activities.activator_id = employees.id)\
                                         as total_sales FROM employees JOIN branches ON employees.branche = branches.id')
-    # decode the binary strings in the second column
     table_data = [[val.decode() if isinstance(val, bytes) else val for val in row] for row in employees_data]
     table_data = [[0 if val is None else val for val in row] for row in table_data]
     for row in sorted(table_data, key=lambda x: x[0]):
         print(" ".join([str(x) for x in row]))
+
+    # 3. print a detailed activity report
+    print('\n' + "Activities report")
+    activities_report = repo.execute_command('SELECT activities.date, products.description, activities.quantity,\
+                                         (SELECT employees.name FROM employees WHERE activities.activator_id = employees.id) as seller_name,\
+                                        (SELECT suppliers.name FROM suppliers WHERE activities.activator_id = suppliers.id) as supplier_name\
+                                        FROM activities JOIN products ON activities.product_id = products.id')
+    table_data = [[val.decode() if isinstance(val, bytes) else val for val in row] for row in activities_report]
+    for row in sorted(table_data, key=lambda x: x[0]):
+        print(tuple(row))
 
 
 if __name__ == '__main__':
