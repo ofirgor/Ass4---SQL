8b604c0339f9f8bb0ba3e50b9e87f997f390c084
ofirgor <118345817+ofirgor@users.noreply.github.com>
2023-01-20 10:43:20 +0200

    implemented second action in printdb.py: printing employees report

--- printdb.py
+++ printdb.py
@@ -1,7 +1,8 @@
 from persistence import *
 
 def main():
-    #TODO: implement
+    # print all tables
+
     tables_names = {k: v for k, v in repo.__dict__.items() if isinstance(v, Dao)}
     for table in sorted(tables_names):
         table_name = table.capitalize()
@@ -11,6 +12,18 @@ def main():
         table_data = [[val.decode() if isinstance(val, bytes) else val for val in row] for row in table_data]
         for row in table_data:
             print(tuple(row))
+    # print employees report
+
+    print("Employees report")
+    employees_data = repo.execute_command('SELECT employees.name, employees.salary, branches.location\
+                                        , (SELECT SUM(-quantity* (SELECT price FROM products WHERE products.id = activities.product_id))\
+                                        FROM activities WHERE activities.activator_id = employees.id)\
+                                        as total_sales FROM employees JOIN branches ON employees.branche = branches.id')
+    # decode the binary strings in the second column
+    table_data = [[val.decode() if isinstance(val, bytes) else val for val in row] for row in employees_data]
+    table_data = [[0 if val is None else val for val in row] for row in table_data]
+    for row in sorted(table_data, key=lambda x: x[0]):
+        print(" ".join([str(x) for x in row]))
 
 
 if __name__ == '__main__':
